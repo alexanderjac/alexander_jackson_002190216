ec1fee1e1e95db36abcdc9bb1f58658ef488f3d5
alexanderjac <alexander.jac@northeastern.edu>
2021-09-26 21:18:47 -0400

    assignment 1 complete

---
+++ src/Model/Person.java
@@ -0,0 +1,201 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+import java.util.Date;
+
+/**
+ *
+ * @author jacksonalexander
+ */
+public class Person {
+    private String name;
+    private String country;
+    private String city;
+    private String street;
+    private Date dateOfbirth ;
+    private String telePhone;
+    private String faxNumber;
+    private String emailId;
+    private long ssn;
+    private String medRecnum;
+    private String healthPlanBenenum;
+    private String bankAccnum;
+    private String certNum;
+    private String vehIden;
+    private String deviceIden;
+    private String linkedIn;
+    private String internetProtocolAddress;
+    private String photo;
+    private String biometric;
+    private String uniqueIdentifyingNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Date getDateOfbirth() {
+        return dateOfbirth;
+    }
+
+    public void setDateOfbirth(Date dateOfbirth) {
+        this.dateOfbirth = dateOfbirth;
+    }
+
+    public String getTelePhone() {
+        return telePhone;
+    }
+
+    public void setTelePhone(String telePhone) {
+        this.telePhone = telePhone;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedRecnum() {
+        return medRecnum;
+    }
+
+    public void setMedRecnum(String medRecnum) {
+        this.medRecnum = medRecnum;
+    }
+
+    public String getHealthPlanBenenum() {
+        return healthPlanBenenum;
+    }
+
+    public void setHealthPlanBenenum(String healthPlanBenenum) {
+        this.healthPlanBenenum = healthPlanBenenum;
+    }
+
+    public String getBankAccnum() {
+        return bankAccnum;
+    }
+
+    public void setBankAccnum(String bankAccnum) {
+        this.bankAccnum = bankAccnum;
+    }
+
+    public String getCertNum() {
+        return certNum;
+    }
+
+    public void setCertNum(String certNum) {
+        this.certNum = certNum;
+    }
+
+    public String getVehIden() {
+        return vehIden;
+    }
+
+    public void setVehIden(String vehIden) {
+        this.vehIden = vehIden;
+    }
+
+    public String getDeviceIden() {
+        return deviceIden;
+    }
+
+    public void setDeviceIden(String deviceIden) {
+        this.deviceIden = deviceIden;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getInternetProtocolAddress() {
+        return internetProtocolAddress;
+    }
+
+    public void setInternetProtocolAddress(String internetProtocolAddress) {
+        this.internetProtocolAddress = internetProtocolAddress;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    
+    
+    public String getUniqueIdentifyingNum() {
+        return uniqueIdentifyingNum;
+    }
+
+    public void setUniqueIdentifyingNum(String uniqueIdentifyingNum) {
+        this.uniqueIdentifyingNum = uniqueIdentifyingNum;
+    }
+
+   
+    
+    
+}
